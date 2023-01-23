3b6d1e16bfe4d695d9db5268927b7f14b63d9ec5
Egor Komarov <egor.komarov@bf.rocks>
2023-01-23 16:54:16 +0100

    add onboarding theme

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,23 @@ import '../style.css'
 import LedgerBridge from './ledger-bridge'
 
 (() => {
+    const params = new URLSearchParams(window.location.search)
+    const theme = params.get('theme') || 'default'
+    const container = document.querySelector(`[data-theme="${theme}"]`)
+
+    container.style.display = null
+
     const bridge = new LedgerBridge()
 
     // User gesture
-    const button = document.getElementById('ledger-pairing-btn')
-    if (button != null) {
-        button.addEventListener('click', () => bridge.getConfiguration())
+    const pairing = container.querySelector('[data-action="pairing"]')
+    if (pairing) {
+        pairing.addEventListener('click', () => bridge.getConfiguration())
+    }
+
+    const back = container.querySelector('[data-action="back"]')
+    if (back) {
+        back.addEventListener('click', () => bridge.cleanUp('ledger-bridge-back'))
     }
 
     console.log(`EVER Wallet <=> Ledger Bridge initialized from ${window.location}!`)
